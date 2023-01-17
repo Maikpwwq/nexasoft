fd918ff77aa57b9005e01fb0a828a8c81fc6b6bd
Maikpwwq <mariasf@correo.udistrital.edu.co>
2023-01-17 03:38:09 -0500

    fix: imports

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { component$ } from "@builder.io/qwik";
-import { qwikify$ } from "@builder.io/qwik-react";
+// import { qwikify$ } from "@builder.io/qwik-react";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import { Link } from "@builder.io/qwik-city";
+// import { Link } from "@builder.io/qwik-city";
 
 import Contact from "../components/contact/contact";
 import Footer from "../components/footer/footer";
