08d8d5fcfc5738d4fde513d2059db1a8b064475e
Maikpwwq <mariasf@correo.udistrital.edu.co>
2023-06-20 16:58:49 -0500

    feat: disagregate init mongo atlas

--- src/routes/customer-record/index.tsx
+++ src/routes/customer-record/index.tsx
@@ -1,21 +1,7 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-// import mongoose from "mongoose"; // ,
-// import * as dotenv from "dotenv";
+// /* eslint-disable @typescript-eslint/no-var-requires */
 // import { type RequestHandler } from "@builder.io/qwik-city";
-
 // // import { component$ } from "@builder.io/qwik";
-// dotenv.config();
-
-// // Variables de entorno
-// const DB_USER = `${process.env.VITE_DB_USER}`;
-// // const DB_PASSWORD = `${process.env.VITE_DB_PASSWORD}`;
-// const DB_PASSWORD2 = `${process.env.VITE_DB_PASSWORD2}`;
-// const DB_NAME = `${process.env.VITE_DB_NAME}`;
-// const MONGODB_COLLECTION = `${process.env.VITE_MONGODB_COLLECTION}`;
-// // const DB_HOST = `${process.env.VITE_DB_HOST}`;
-// // const MONGO_HOST = `mongodb+srv://${DB_USER}:${DB_PASSWORD2}@${DB_HOST}/?retryWrites=true&w=majority`;
-// const MONGO_HOST = `${process.env.VITE_MONGO_HOST}`;
-// // console.log("MONGO_HOST:", MONGO_HOST);
+// import { Contactos } from "~/services/mongo-init";
 
 // interface RegistroContacto {
 //   name: String;
@@ -24,26 +10,6 @@
 //   issue: String;
 //   message: String;
 // }
-
-// const schema = new mongoose.Schema({
-//   name: String,
-//   email: String,
-//   phone: String,
-//   issue: String,
-//   message: String,
-// });
-
-// const options = {
-//   dbName: DB_NAME,
-//   user: DB_USER,
-//   pass: DB_PASSWORD2,
-//   // useNewUrlParser: true,
-//   // useUnifiedTopology: true,
-// };
-
-// const clientPromise = mongoose.createConnection(MONGO_HOST, options);
-
-// const Contactos = clientPromise.model(MONGODB_COLLECTION, schema);
 
 // // Called when this HTTP method is POST
 // // onRequest, onGet, onPost, onPut, onDelete, onPatch, onHead
@@ -77,7 +43,6 @@
 //   } catch (err: any) {
 //     console.error(
 //       "[Error when trying to create new customer record]",
-//       MONGO_HOST,
 //       err
 //     );
 //     // if (err === "MongoNotConnectedError: Client must be connected before running operations")
---
+++ src/services/mongo-init.js
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import mongoose from "mongoose"; // ,
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+// Variables de entorno
+const DB_USER = `${process.env.VITE_DB_USER}`;
+// const DB_PASSWORD = `${process.env.VITE_DB_PASSWORD}`;
+const DB_PASSWORD2 = `${process.env.VITE_DB_PASSWORD2}`;
+const DB_NAME = `${process.env.VITE_DB_NAME}`;
+const MONGODB_COLLECTION = `${process.env.VITE_MONGODB_COLLECTION}`;
+// const DB_HOST = `${process.env.VITE_DB_HOST}`;
+// const MONGO_HOST = `mongodb+srv://${DB_USER}:${DB_PASSWORD2}@${DB_HOST}/?retryWrites=true&w=majority`;
+const MONGO_HOST = `${process.env.VITE_MONGO_HOST}`;
+// console.log("MONGO_HOST:", MONGO_HOST);
+
+
+const schema = new mongoose.Schema({
+    name: String,
+    email: String,
+    phone: String,
+    issue: String,
+    message: String,
+  });
+  
+  const options = {
+    dbName: DB_NAME,
+    user: DB_USER,
+    pass: DB_PASSWORD2,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  };
+
+
+const clientPromise = mongoose.createConnection(MONGO_HOST, options);
+
+const Contactos = clientPromise.model(MONGODB_COLLECTION, schema);
+
+export { Contactos }
